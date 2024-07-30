d0904cece218e91d9bbdceee8b2c66fb9329ac3f
ayrton <ayrton.parkinson1@gmail.com>
2024-07-30 06:29:34 -0400

    Add ability to change mapstyle

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var viewModel = ViewModel()
+    @State private var mapStyle = MapStyle.standard
+    @State private var showingMapStyle = false
     
     let startPosition = MapCameraPosition.region(
         MKCoordinateRegion(
@@ -18,35 +20,52 @@ struct ContentView: View {
             span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
         )
     )
-    
+
     var body: some View {
         if viewModel.isUnlocked {
-            MapReader { proxy in
-                Map(initialPosition: startPosition) {
-                    ForEach(viewModel.locations) { location in
-                        Annotation(location.name, coordinate: location.coordinate) {
-                            Image(systemName: "star.circle")
-                                .resizable()
-                                .foregroundStyle(.red)
-                                .frame(width: 44, height: 44)
-                                .background(.white)
-                                .clipShape(.circle)
-                                .onLongPressGesture {
-                                    viewModel.selectedPlace = location
-                                }
+            ZStack(alignment: .topTrailing) {
+                MapReader { proxy in
+                    Map(initialPosition: startPosition) {
+                        ForEach(viewModel.locations) { location in
+                            Annotation(location.name, coordinate: location.coordinate) {
+                                Image(systemName: "star.circle")
+                                    .resizable()
+                                    .foregroundStyle(.red)
+                                    .frame(width: 44, height: 44)
+                                    .background(.white)
+                                    .clipShape(.circle)
+                                    .onLongPressGesture {
+                                        viewModel.selectedPlace = location
+                                    }
+                            }
                         }
                     }
-                }
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        viewModel.addLocation(at: coordinate)
+                    .mapStyle(mapStyle)
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            viewModel.addLocation(at: coordinate)
+                        }
+                    }
+                    .sheet(item: $viewModel.selectedPlace) { place in
+                        EditView(location: place) {
+                            viewModel.update(location: $0)
+                        }
+                    }
+                    .confirmationDialog("Select map style", isPresented: $showingMapStyle) {
+                        Button("Standard") { mapStyle = .standard }
+                        Button("Imagery") { mapStyle = .imagery }
+                        Button("Hybrid") { mapStyle = .hybrid }
                     }
                 }
-                .sheet(item: $viewModel.selectedPlace) { place in
-                    EditView(location: place) {
-                        viewModel.update(location: $0)
-                    }
+                Button {
+                    showingMapStyle = true
+                } label: {
+                    Image(systemName: "map.fill")
+                        .padding()
+                        .background(.white)
+                        .clipShape(.circle)
                 }
+                .padding(.trailing)
             }
         } else {
             Button("Unlock Places", action: viewModel.authenticate)
